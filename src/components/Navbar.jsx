18fe63e2f1ced64596ce63ce53453bb392c19e63
Soe Thandar Lwin <soethandarlwin@Soes-MacBook-Air.local>
2023-03-26 10:54:31 +0700

    Initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import './Navbar.css';
+
+function Navbar() {
+    return (
+        <div>
+            <h1 className="task">Tasks</h1>
+            <nav>
+                <li>Incoming</li>
+                <li>Today</li>
+                <li>Calendar</li>
+            </nav>
+        </div>
+    )
+}
+
+export default Navbar;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,13 @@
+import "./Profile.css";
+
+function Profile(){
+    return (
+        <div className="layout">
+            <img src="assets/me.jpeg" alt="stl" />
+            <h1 className="nameName">Casper</h1>
+            <hr></hr>
+        </div>
+    )
+}
+
+export default Profile;
